0a9a8293da9bc94c71fb41e592a25468103b7999
Luiz Ruiz <luizruiz04@gmail.com>
2022-04-21 10:22:25 -0400

    Communication between frontend and backend implemented

---
+++ loja_virtual/loja_virtual/product/serializers.py
@@ -0,0 +1,17 @@
+from dataclasses import fields
+from rest_framework import serializers
+
+from .models import Category, Products
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Products
+        fields = (
+            "id",
+            "name",
+            "get_absolute_url",
+            "description",
+            "price",
+            "get_image",
+            "get_thumbnail"
+        )
--- loja_virtual/loja_virtual_vue/src/App.vue
+++ loja_virtual/loja_virtual_vue/src/App.vue
@@ -4,14 +4,14 @@
       <div class="navbar-brand">
         <router-link to="/" class="navbar-item"><strong>Loja Virtual</strong></router-link>
 
-        <a class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbar-menu">
+        <a class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbar-menu" @click="showMobileMenu = !showMobileMenu">
           <span aria-hidden="true"></span>
           <span aria-hidden="true"></span>
           <span aria-hidden="true"></span>
         </a>
       </div>
 
-      <div class="navbar-menu" id="navbar-menu">
+      <div class="navbar-menu" id="navbar-menu" v-bind:class="{'is-active': showMobileMenu }">
         <div class="navbar-end">
           <div class="navbar-item">
             <div class="buttons">
@@ -36,6 +36,16 @@
     </footer>
   </div>
 </template>
+
+<script>
+export default{
+  data() {
+    return{
+      showMobileMenu: false,
+    }
+  }
+}
+</script>
 
 <style lang="scss">
 @import '../node_modules/bulma';
