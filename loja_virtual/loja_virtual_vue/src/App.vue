c049432db87187018ecfb6a5ad1195774e9d37e5
Luiz Ruiz <luizruiz04@gmail.com>
2022-04-20 21:11:49 -0400

    Vue and Django projects started

---
+++ loja_virtual/loja_virtual_vue/src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="wrapper">
+    <nav class="navbar is-dark">
+      <div class="navbar-brand">
+        <router-link to="/" class="navbar-item"><strong>Loja Virtual</strong></router-link>
+
+        <a class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbar-menu">
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+        </a>
+      </div>
+
+      <div class="navbar-menu" id="navbar-menu">
+        <div class="navbar-end">
+          <div class="navbar-item">
+            <div class="buttons">
+              <router-link to="/log-in" class="button is-light">Log in</router-link>
+
+              <router-link to="/cart" class="button is-success">
+                <span class="icon"><i class="fas fa-shopping-cart"></i></span>
+                <span>Carrinho</span>
+              </router-link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </nav>
+
+    <sectrion class="section">
+      <router-view/>
+    </sectrion>
+    
+    <footer class="footer">
+      <p class="has-text-centered">Copyright (c) 2022</p>
+    </footer>
+  </div>
+</template>
+
+<style lang="scss">
+@import '../node_modules/bulma';
+</style>
